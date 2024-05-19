26f5ee974265fd5ebcf12411f230ac52a0573890
Aravind_k_v <aravindkvishwanath59@gmail.com>
2024-05-19 21:05:30 +0530

    APK update

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,18 @@
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['module:metro-react-native-babel-preset'],
+//     plugins: ["@babel/plugin-transform-private-methods"]
+//   };
+// };
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ],
   };
 };
